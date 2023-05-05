430b1b59356ced2e14baf1a6d76ff1d3b104b7c8
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2023-05-05 00:46:08 -0400

    Update yuzu-update.sh

--- yuzu-update.sh
+++ yuzu-update.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Your yuzu folder, hopefully
-YUZU_DIR="~/.local/share/yuzu"
+YUZU_DIR="/home/deck/.local/share/yuzu"
 # Get latest version (yellows8 my beloved)
 FW_VERSION=$(curl -s https://yls8.mtheall.com/ninupdates/feed.php | grep -m 1 "Switch" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
 
+echo "Yuzu Updater Script V1.0"
+
 # Grab latest EA Yuzu
-curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO "/home/deck/Applications/Yuzu.AppImage" --show-progress -i -
+curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO "/home/deck/Applications/Yuzu.AppImage" -i - && echo "Downloaded latest Yuzu AppImage"
 chmod +x "/home/deck/Applications/Yuzu.AppImage"
 
 # Get latest keys (mind the URL)
-wget -qO "$YUZU_DIR/keys/prod.keys" --show-progress "https://sigmapatches.coomer.party/prod.keys"
+wget -qO "$YUZU_DIR/keys/prod.keys" "https://sigmapatches.coomer.party/prod.keys" && echo "Downloaded keys for $FW_VERSION"
 
 # Get latest firmware and extract it
-wget -qO "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" --show-progress "https://archive.org/download/nintendo-switch-global-firmwares/Firmware%20$FW_VERSION.zip"
+wget -qO "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" "https://archive.org/download/nintendo-switch-global-firmwares/Firmware%20$FW_VERSION.zip" && echo "Downloaded firmware $FW_VERSION"
 
-unzip -q "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" -d "$YUZU_DIR/nand/system/Contents/registered/"
+rm -f $YUZU_DIR/nand/system/Contents/registered/*.nca
+unzip -q "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" -d "$YUZU_DIR/nand/system/Contents/registered/" && echo "Extracted firmware"
 rm "$YUZU_DIR/nand/system/Contents/registered/firmware.zip"
