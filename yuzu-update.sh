5a3abfc39da03657967f2dfd24d418d04eb3d9e2
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2023-05-05 00:09:06 -0400

    fix shit again

--- yuzu-update.sh
+++ yuzu-update.sh
@@ -3,11 +3,11 @@
 # Your yuzu folder, hopefully
 YUZU_DIR="~/.local/share/yuzu"
 # Get latest version (yellows8 my beloved)
-FW_VERSION=$(curl https://yls8.mtheall.com/ninupdates/feed.php | grep -m 1 "Switch" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+FW_VERSION=$(curl -s https://yls8.mtheall.com/ninupdates/feed.php | grep -m 1 "Switch" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
 
 # Grab latest EA Yuzu
-curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ~/Applications/Yuzu.AppImage --show-progress -i -
-chmod +x ~/Applications/Yuzu.AppImage
+curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO "/home/deck/Applications/Yuzu.AppImage" --show-progress -i -
+chmod +x "/home/deck/Applications/Yuzu.AppImage"
 
 # Get latest keys (mind the URL)
 wget -qO "$YUZU_DIR/keys/prod.keys" --show-progress "https://sigmapatches.coomer.party/prod.keys"
@@ -15,5 +15,5 @@ wget -qO "$YUZU_DIR/keys/prod.keys" --show-progress "https://sigmapatches.coomer
 # Get latest firmware and extract it
 wget -qO "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" --show-progress "https://archive.org/download/nintendo-switch-global-firmwares/Firmware%20$FW_VERSION.zip"
 
-unzip -q $YUZU_DIR/nand/system/Contents/registered/firmware.zip -d $YUZU_DIR/nand/system/Contents/registered/
-rm $YUZU_DIR/nand/system/Contents/registered/firmware.zip
+unzip -q "$YUZU_DIR/nand/system/Contents/registered/firmware.zip" -d "$YUZU_DIR/nand/system/Contents/registered/"
+rm "$YUZU_DIR/nand/system/Contents/registered/firmware.zip"
